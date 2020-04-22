2f6ffad78ffbc5ae9ad6673023a1880e22b1083a
Jay Honnold <jayhonnold@gmail.com>
2020-04-22 10:15:27 -0500

    Updates

---
+++ precache-manifest.3538d901b15511ff50f67ec824bc2146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7494a5866a6a3fce5222b487aab2cb8c",
+    "url": "/react-chart-js/index.html"
+  },
+  {
+    "revision": "53c6b61d6094d6c71aff",
+    "url": "/react-chart-js/static/css/main.1f3e3be9.chunk.css"
+  },
+  {
+    "revision": "2e886bbf3473e531f8dd",
+    "url": "/react-chart-js/static/js/2.2eed461c.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-chart-js/static/js/2.2eed461c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c6b61d6094d6c71aff",
+    "url": "/react-chart-js/static/js/main.f2e95d50.chunk.js"
+  },
+  {
+    "revision": "c805999460c1191f1924",
+    "url": "/react-chart-js/static/js/runtime-main.4adbc820.js"
+  }
+]);
